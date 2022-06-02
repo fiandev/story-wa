341dc1610f3b61a6e216245d606bfacaabf3516f
fiandev <wongkeras2@gmail.com>
2022-06-02 13:33:46 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cli = process.argv
 const { changeStory, backupStory } = require("./controllers");
-//console.log(process.argv);
+
 if (cli.includes("change", 0)) {
   changeStory()
 } else if(cli.includes("backup", 0)) {
